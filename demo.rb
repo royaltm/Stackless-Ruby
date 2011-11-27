b8e4163189ff13577f7c111da06aa31e008b3bed
Rafał Michalski <royaltm75@gmail.com>
2011-11-27 17:32:56 +0100

    fix tabs2spaces

--- demo.rb
+++ demo.rb
@@ -1,37 +1,37 @@
 require './stackless.rb'
 
 def timer
-	start = Time.now
-	ret = yield
-	puts "#{Time.now - start}s"
-	ret
+  start = Time.now
+  ret = yield
+  puts "#{Time.now - start}s"
+  ret
 end
 
 module Math
-	def self.power(x)
-		x.zero? ? 1 : x * power(x.abs - 1)
-	end
+  def self.power(x)
+    x.zero? ? 1 : x * power(x.abs - 1)
+  end
 end
 
 def test_eval cmd
-	print "#{cmd}: "
-	eval cmd
+  print "#{cmd}: "
+  eval cmd
 end
 
 def demo
-	begin
-		x = 0
-		loop { test_eval "timer { Math.power(#{x+=1000}) }" }
-	rescue SystemStackError
-		puts "ups...#{$!.inspect}"
-		print "press enter"
-		gets
-		class << Math
-			stackless_method :power
-		end
-		puts "\nBehold the Power Of The Stackless!"
-		retry
-	end
+  begin
+    x = 0
+    loop { test_eval "timer { Math.power(#{x+=1000}) }" }
+  rescue SystemStackError
+    puts "ups...#{$!.inspect}"
+    print "press enter"
+    gets
+    class << Math
+      stackless_method :power
+    end
+    puts "\nBehold the Power Of The Stackless!"
+    retry
+  end
 end
 
 demo
--- stackless.rb
+++ stackless.rb
@@ -31,36 +31,36 @@ require 'fiber'
 # class << modulename; stackless_method :method_name; end
 #
 class Module
-	NoStacklessMethodSuffix = '__no_stackless'
+  NoStacklessMethodSuffix = '__no_stackless'
   def stackless_method(*names)
-		allow_stack = Numeric===names.last ? names.pop : 200
-		names.each do |name|
-			raise NameError, "method `#{name}' for class `#{self}'" \
-				" is already stackless" if stackless_method? name
-			stk_name = (name.to_s + ::Module::NoStacklessMethodSuffix).intern
-			alias_method stk_name, name
-			define_method(name) do |*args, &blk|
-				if caller(allow_stack).nil?
-					send stk_name, *args, &blk
-				else
-					Fiber.new(&method(stk_name)).resume(*args, &blk)
-				end
-			end
-		end
+    allow_stack = Numeric===names.last ? names.pop : 200
+    names.each do |name|
+      raise NameError, "method `#{name}' for class `#{self}'" \
+        " is already stackless" if stackless_method? name
+      stk_name = (name.to_s + ::Module::NoStacklessMethodSuffix).intern
+      alias_method stk_name, name
+      define_method(name) do |*args, &blk|
+        if caller(allow_stack).nil?
+          send stk_name, *args, &blk
+        else
+          Fiber.new(&method(stk_name)).resume(*args, &blk)
+        end
+      end
+    end
   end
 
-	def stackless_method?(name)
-		method_defined?((name.to_s + ::Module::NoStacklessMethodSuffix).intern)
-	end
+  def stackless_method?(name)
+    method_defined?((name.to_s + ::Module::NoStacklessMethodSuffix).intern)
+  end
 
-	def remove_stackless(*names)
-		names.each do |name|
-			raise NameError, "unknown stackless method `#{name}' " \
-				"for class `#{self}'" unless stackless_method? name
-			stk_name = (name.to_s + ::Module::NoStacklessMethodSuffix).intern
-			alias_method name, stk_name
-			remove_method stk_name
-		end
-	end
+  def remove_stackless(*names)
+    names.each do |name|
+      raise NameError, "unknown stackless method `#{name}' " \
+        "for class `#{self}'" unless stackless_method? name
+      stk_name = (name.to_s + ::Module::NoStacklessMethodSuffix).intern
+      alias_method name, stk_name
+      remove_method stk_name
+    end
+  end
 end
 # enjoy...
